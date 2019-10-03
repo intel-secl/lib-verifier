4b5dec8e0fe09770377fb144ac298fa5f788a7bf
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2019-10-03 07:07:56 -0700

    update tag rule

--- verifier/src/main/java/com/intel/mtwilson/core/verifier/policy/rule/TagCertificateTrusted.java
+++ verifier/src/main/java/com/intel/mtwilson/core/verifier/policy/rule/TagCertificateTrusted.java
@@ -48,7 +48,7 @@ public class TagCertificateTrusted extends BaseRule {
             for (int i = 0; i < trustedAuthorityCerts.length && !validCaSignature; i++) {
                 X509Certificate ca = trustedAuthorityCerts[i];
                 try {
-                    if (x509AttributeCertificate.getIssuer().equalsIgnoreCase(ca.getIssuerX500Principal().getName())) {
+                    if (x509AttributeCertificate.getIssuer().equalsIgnoreCase(ca.getSubjectDN().getName())) {
                         if (x509AttributeCertificate.isValid(ca)) {
                             // NOTE:  CA certificate must be valid for the start date and end date of the tag certificate's validity - we don't let a CA generate certs for a period when the CA itself is expired.
                             //        if this rule is too strict in practice we can remove it
@@ -61,7 +61,7 @@ public class TagCertificateTrusted extends BaseRule {
                             log.debug("TagCertificate is not valid");
                         }
                     } else {
-                        log.debug("Issuer name mismatch : {} vs {}", x509AttributeCertificate.getIssuer(), ca.getIssuerX500Principal().getName());
+                        log.debug("Issuer name mismatch : {} vs {}", x509AttributeCertificate.getIssuer(), ca.getSubjectDN().getName());
                     }
                 } catch (Exception e) { //CertificateExpiredException | CertificateNotYetValidException e) {
                     log.debug("Failed to verify tag signature with CA: {}", e.getMessage()); // suppressing because maybe another cert in the list is a valid signer
